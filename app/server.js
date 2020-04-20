c72accb95d0c2d1da350fefcaaed676fd3066465
BhupinderS24 <ikwvibe1@gmail.com>
2020-04-20 12:57:28 +0530

    cred

--- app/server.js
+++ app/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
 app.use((req,res,next)=>{
-    res.header('Access-control-Allow-Origin','http://localhost:4200');
+    res.header('Access-Control-Allow-Origin','http://localhost:4200');
     res.header(
         "Access-Control-Allow-Headers",
         "*"
@@ -25,8 +25,9 @@ app.use((req,res,next)=>{
     // res.header('Access-Control-Allow-Credentials','true');
     // res.header('Access-Control-Allow-Methods','*');
     if(req.method==='OPTIONS'){
-        res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
-        res.header('Access-Control-Allow-Credentials','true');
+
+        res.header('Access-Control-Allow-Headers','*');
+        res.header('Access-Control-Allow-Origin','http://localhost:4200');
         // res.header(
         //     "Access-Control-Allow-Headers",
         //     "*"
