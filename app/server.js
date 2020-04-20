5576f4a560f2997ddb6462299873c2ecbefa1438
BhupinderS24 <ikwvibe1@gmail.com>
2020-04-20 14:04:02 +0530

    cred

--- app/server.js
+++ app/server.js
@@ -14,7 +14,7 @@ app.use((req,res,next)=>{
     res.header('Access-Control-Allow-Origin',req.headers.origin);
     res.header(
         "Access-Control-Allow-Headers",
-        '*'
+        'Content-type'
     );
     res.header('Access-Control-Allow-Credentials','true');
     // res.header('Access-Control-Allow-Methods','*');
