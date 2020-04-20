f1ce31fc1a48f1c094bef2aadc111d47e2bc9ba4
BhupinderS24 <ikwvibe1@gmail.com>
2020-04-20 13:06:15 +0530

    cred

--- app/server.js
+++ app/server.js
@@ -10,14 +10,8 @@ var cookieParser = require('cookie-parser');
 var mongoDB = process.env.MONGODB_URI;
 
 mongoose.connect(mongoDB , {useNewUrlParser: true});
-
-app.use(cookieParser());
-app.use(logger);
-app.use(express.json());
-app.use(express.urlencoded({extended:false}));
-
 app.use((req,res,next)=>{
-    res.header('Access-Control-Allow-Origin','http://localhost:4200');
+    res.header('Access-Control-Allow-Origin',req.headers.origin);
     res.header(
         "Access-Control-Allow-Headers",
         "*"
@@ -26,7 +20,7 @@ app.use((req,res,next)=>{
     // res.header('Access-Control-Allow-Methods','*');
     if(req.method==='OPTIONS'){
 
-        res.header('Access-Control-Allow-Headers','*');
+        res.header('Access-Control-Allow-Headers',req.headers.origin);
         res.header('Access-Control-Allow-Origin','http://localhost:4200');
         // res.header(
         //     "Access-Control-Allow-Headers",
@@ -37,6 +31,13 @@ app.use((req,res,next)=>{
     }
     next();
 })
+
+app.use(cookieParser());
+app.use(logger);
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+
 
 app.use('/chatcord', require('./routes/authentication.js'));
 
